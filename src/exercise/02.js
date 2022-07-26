d2e0508316e45e1b41d3686ab2c7462d582831a0
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-07-26 22:12:09 +0300

    workshop complete

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,57 +1,80 @@
 import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 
-/* ✅ modify this usePokemon custom hook to take in a query as an argument */
-export function usePokemon() {
-  /* ✅ this hook should only return one thing: an object with the pokemon data */
-}
 
-function Pokemon({ query }) {
-  /* 
-   ✅ move the code from the useState and useEffect hooks into the usePokemon hook
-   then, call the usePokemon hook to access the pokemon data in this component
-  */
-  const [pokemon, setPokemon] = useState(null);
-  useEffect(() => {
+export function usePokemon(query) {
+   const [data, setData] = useState(null);
+   const [status, setStatus] = useState("idle");
+  const [ errors, setErrors ] = useState( null );
+  
+   useEffect(() => {
+    setStatus("pending")
     fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
-      .then(r => r.json())
-      .then(setPokemon);
-  }, [query]);
-
-  // 🚫 don't worry about the code below here, you shouldn't have to touch it
-  if (!pokemon) return <h3>Loading...</h3>;
-
+      .then(r => {
+        if(r.ok){
+          return r.json()
+        }else{
+          return r.text().then(err=>{
+            throw err
+          })
+        }
+      } )
+     .then(data=>{
+        setData(data)
+        setStatus("fulfilled")
+      })
+      .catch(err=>{
+        console.log(err)
+        setStatus("rejected")
+        setErrors([err])
+      });
+   }, [ query ] );
+   return {data: data, status, errors}
+}
+function Pokemon({ query }) {
+  const { data: pokemon, status, errors } = usePokemon( query );
+  if (status === "idle" || status === "pending") return <h3>Loading...</h3>;
+  if ( status === "rejected" )
+  {
+     return (
+       <div>
+         <h3>Error</h3>
+         {errors.map(err => (
+           <p key={err}>{err}</p>
+         ))}
+       </div>
+     );
+  }
   return (
     <div>
       <h3>{pokemon.name}</h3>
       <img
         src={pokemon.sprites.front_default}
         alt={pokemon.name + " front sprite"}
-      />
-    </div>
-  );
-}
-
-export default function App() {
-  const [query, setQuery] = useState("charmander");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setQuery(e.target.search.value);
-  }
-
-  return (
-    <Wrapper>
-      <h1>PokéSearcher</h1>
-      <Pokemon query={query} />
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="search" defaultValue={query} />
+        />
+        </div>
+      );
+    }
+    
+    export default function App() {
+      const [query, setQuery] = useState("charmander");
+    
+      function handleSubmit(e) {
+        e.preventDefault();
+        setQuery(e.target.search.value);
+      }
+    
+      return (
+        <Wrapper>
+          <h1>PokéSearcher</h1>
+          <Pokemon query={query} />
+          <form onSubmit={handleSubmit}>
+          <input type="text" name="search" defaultValue={query} />
         <button type="submit">Search</button>
       </form>
     </Wrapper>
   );
 }
-
 const Wrapper = styled.section`
   box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.15);
   display: grid;
@@ -60,7 +83,6 @@ const Wrapper = styled.section`
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   background: papayawhip;
   text-align: center;
-
   h1 {
     background: #ef5350;
     color: white;
@@ -70,7 +92,6 @@ const Wrapper = styled.section`
     color: white;
     font-size: 2rem;
   }
-
   form {
     display: grid;
     grid-template-columns: 1fr auto;
